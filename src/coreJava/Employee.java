9745d73aaf4c8c13977cad4a17be691ccb27a87d
Priyanka muppuri <priyanka.muppuri@hotmail.com>
2017-09-23 08:50:12 -0700

    Update Employee.java

--- src/coreJava/Employee.java
+++ src/coreJava/Employee.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+// employee model
 public class Employee implements EmployeeInterface{
 	
 	private int empId;
